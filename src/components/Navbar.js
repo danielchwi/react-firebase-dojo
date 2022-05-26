672c8fffe732d9cf728c50f1a9c8565ee03e85be
danielchwi <daniel.widodo@bitcoin.co.id>
2022-05-26 21:03:25 +0700

    Login func setup form and online status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 import './Navbar.css'
 import Temple from '../assets/temple.svg'
 
 export default function Navbar() {
+    const { logout, isLoading } = useLogout()
   return (
     <div className='navbar'>
         <ul>
@@ -17,9 +19,8 @@ export default function Navbar() {
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/signup">Signup</Link></li>
             <li>
-                <button className='btn'>
-                    Logout 
-                </button>
+                {!isLoading && <button className='btn' onClick={logout}>Logout</button>}
+                {isLoading && <button className='btn' disabled>Logging out</button>}
             </li>
 
         </ul>
