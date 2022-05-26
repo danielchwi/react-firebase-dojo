8379ebc3713ed812336c17d0545eb74d8ad218ef
danielchwi <daniel.widodo@bitcoin.co.id>
2022-05-26 21:52:25 +0700

    Setup sidebar conditional rendering, make avatar components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 import Dashboard from './pages/dashboard/Dashboard'
 import Create from './pages/create/Create'
 import Login from './pages/login/Login'
@@ -9,48 +10,55 @@ import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 
 function App() {
+  const { user, authIsReady } = useAuthContext()
   return (
     <div className="App">
-      
-      <BrowserRouter>
-      <Sidebar/>
-        <div className='container'>
-        <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-          </Switch>
+      {authIsReady && (
+        <BrowserRouter>
+        {user && <Sidebar/>}
+          <div className='container'>
+          <Navbar />
+            <Switch>
+              <Route exact path="/">
+                {!user && <Redirect to="/login"/>}
+                {user && <Dashboard />}
+              </Route>
+            </Switch>
 
-          <Switch>
-            <Route path="/create">
-              <Create />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route path="/create">
+                {!user && <Redirect to="/login"/>}
+                {user && <Create />}
+              </Route>
+            </Switch>
 
-          <Switch>
-            <Route exact path="/projects">
-              <Project />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route path="/projects">
+                {!user && <Redirect to="/login"/>}
+                {user && <Project />}
+              </Route>
+            </Switch>
 
-          <Switch>
-            <Route exact path="/signup">
-              <Signup />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route exact path="/signup">
+                {user && <Redirect to="/"/>}
+                {!user && <Signup />}
+              </Route>
+            </Switch>
 
-          <Switch>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route exact path="/login">
+                {user && <Redirect to="/"/>}
+                {!user && <Login />}
+              </Route>
+            </Switch>
 
-        </div>
+          </div>
 
+          
         
-      
-      </BrowserRouter>
+        </BrowserRouter>
+      )}
 
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 import './Navbar.css'
 import Temple from '../assets/temple.svg'
 
 export default function Navbar() {
     const { logout, isLoading } = useLogout()
+    const { user } = useAuthContext()
   return (
     <div className='navbar'>
         <ul>
@@ -15,13 +17,19 @@ export default function Navbar() {
                     <span>The Dojo</span>
                 </Link>
             </li>
-
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-            <li>
-                {!isLoading && <button className='btn' onClick={logout}>Logout</button>}
-                {isLoading && <button className='btn' disabled>Logging out</button>}
-            </li>
+            
+            {!user && (
+                <>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/signup">Signup</Link></li>
+                </>
+            )}
+            {user && (
+                <li>
+                    {!isLoading && <button className='btn' onClick={logout}>Logout</button>}
+                    {isLoading && <button className='btn' disabled>Logging out...</button>}
+                </li>
+            )}
 
         </ul>
     </div>
